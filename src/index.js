df92a5b1f11991f1f3f11ab270d715c7c6b96c72
Eric Souza <eric.bh18souza@gmail.com>
2021-03-25 12:02:26 -0300

    JS logic and CSS styling have been added to project and it has been deployed using netlify

---
+++ src/index.js
@@ -0,0 +1,92 @@
+// Creates elements which will render the game
+let canvas = document.getElementById("snake") 
+let context = canvas.getContext("2d")
+
+let box = 32  
+let snake = [] // Snake set as array, since it is a series of coordinates
+
+snake[0] ={
+    x: 8 * box,
+    y: 8 * box
+}
+
+let direction = "right" // Sets default starting direction
+
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+// Sets game map using specified width and height
+function createMap () {
+    context.fillStyle = "#fafafa"
+    context.fillRect(0, 0, 16 * box, 16 * box) 
+}
+
+// Rendering configurations
+function renderSnake () {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "black"
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+}
+
+function renderFood () {
+    context.fillStyle = "red"
+    context.fillRect(food.x, food.y, box, box)
+}
+
+// Movement configurations
+document.addEventListener('keydown', update)
+
+function update (event) {
+    if (event.keyCode == 37 && direction != 'right') direction = 'left'
+    if (event.keyCode == 38 && direction != 'down') direction = 'up'
+    if (event.keyCode == 39 && direction != 'left') direction = 'right'
+    if (event.keyCode == 40 && direction != 'up') direction = 'down'
+}
+
+// Starting configurations
+function startGame () {    
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0
+    if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0
+    if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box
+    
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(game)
+            alert('Game Over! Update page to restart.')
+        }
+    }
+
+    createMap()
+    renderSnake()
+    renderFood()
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if (direction == "right") snakeX += box
+    if (direction == "left") snakeX -= box
+    if (direction == "up") snakeY -= box
+    if (direction == "down") snakeY += box
+
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop() // Keeps current size if the snake moves without getting food
+    }
+    
+    else {
+        food.x = Math.floor(Math.random() * 15 +1) * box
+        food.y = Math.floor(Math.random() * 15 +1) * box
+    }
+    
+    let addSnakeSize = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(addSnakeSize)
+}
+
+let game = setInterval(startGame, 100)
