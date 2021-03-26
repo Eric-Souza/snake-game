a58ec682b96f1939e450eb9dc85898c53e1bfed7
Eric Souza <eric.bh18souza@gmail.com>
2021-03-26 09:55:59 -0300

    Controls settings implemented and gitignore file added

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ function startGame () {
     for (i = 1; i < snake.length; i++) {
         if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
             clearInterval(game)
-            alert('Game Over! Update page to restart.')
+            alert('Game Over! Refresh page to restart.')
         }
     }
 
